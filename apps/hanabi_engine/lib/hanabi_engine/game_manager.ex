917b277df9bddf43991992b4d8f5d7249140dc16
James Edward Gray II <james@graysoftinc.com>
2017-01-02 15:52:43 -0600

    Reworking the GameManager to use the new modules.

--- apps/hanabi_engine/lib/hanabi_engine/game_manager.ex
+++ apps/hanabi_engine/lib/hanabi_engine/game_manager.ex
@@ -1,43 +1,48 @@
 defmodule HanabiEngine.GameManager do
+  @moduledoc ~S"""
+  This process keeps track of a `Game` in progress.  The process will
+  subscribe to incoming moves and publish updates for each event.
+  """
+
   use GenServer
 
-  defstruct id: nil,
-            status: :setup,
-            players: nil,
-            hands: nil,
-            draw_pile: nil,
-            discards: [ ],
-            fireworks: %{
-              blue: nil,
-              green: nil,
-              red: nil,
-              white: nil,
-              yellow: nil
-            },
-            turn: nil,
-            clocks: 8,
-            fuses: 3
+  defstruct ~w[id game]a
 
   alias Phoenix.PubSub
-  alias HanabiEngine.GameSupervisor
+  alias HanabiEngine.{Game, GameSupervisor, RulesLawyer}
 
   ### Client ###
 
   @doc ~S"""
-  This is the main start interface for client code.
+  Creates the `game_id` and `seed` expected by `start/3`, then forwards to that
+  function.  Returns `{:ok, game_id, players, seed}` or `{:error, message}`.
+  """
+  def start_new(players) do
+    game_id = UUID.uuid1
+    seed = :rand.seed_s(:exsplus) |> :rand.export_seed_s
+    result = start(game_id, players, seed)
+    case result do
+      success when is_tuple(success) and elem(success, 0) == :ok ->
+        {:ok, game_id, players, seed}
+      error ->
+        error
+    end
+  end
 
+  @doc ~S"""
   All `GameManager` processes are dynamically started under a `GameSupervisor`.
   This function handles that sequence for the caller.
 
   A `game_id` is a unique `String` identifier for the game being built.
   Events for the game will be published to a topic beginning
-  `"game:#{game_id}"`.
+  `"game:#{game_id}:events"`.  The `GameManager` subscribes to the topic
+  `"game:#{game_id}:plays"` where it will pick up moves from the players.
 
   `players` is expected to be a `List` of unique identifiers representing
   the players in this game.
 
-  You can provide a `seed` (a `Tuple` of three `Integer` values) to make
-  all randomization the `GameManager` does predictable.  This allows for the
+  You can provide a `seed` (an `export_state()` from `:rand`) to make all
+  randomization the `GameManager` does predictable.  This allows for the
   recreation of games from long term storage.
   """
   def start(game_id, players, seed \\ nil) do
@@ -51,54 +56,38 @@ defmodule HanabiEngine.GameManager do
   end
 
   @doc ~S"""
-  This function triggers the initial deal of player hands and completes
-  `game` setup.
-
-  Details of the setup will be published to each player.
+  Subscribe to receive event messages from the `Game` with `id`.
   """
-  def deal(game) do
-    GenServer.call(game, :deal)
+  def subscribe(id) do
+    PubSub.subscribe(:hanabi, "game:#{id}:events")
   end
 
   @doc ~S"""
-  One of three play functions for the players.
-
-  `game` is the process identifier, `player` is the player making the play
-  (who must be the current player), `to` is the player receiving the hint,
-  and `hint` is either a color atom (like `:yellow`) or a value (like `3`).
-
-  Details of the play will be published to each player.
+  Publishes a request to deal the `Game` with `id`.
   """
-  def hint(game, player, to, hint) do
-    GenServer.call(game, {:hint, player, to, hint})
+  def deal(id) do
+    PubSub.broadcast(:hanabi, "game:#{id}:plays", :deal)
   end
 
   @doc ~S"""
-  One of three play functions for the players.
-
-  `game` is the process identifier, `player` is the player making the play
-  (who must be the current player), and `index` is the zero-based index into
-  the player's hand of the tile they wish to discard.  A fresh tile will be
-  drawn into the player's hand at the same position, if one is available.
-
-  Details of the play will be published to each player.
+  Publishes a hint move the `Game` with `id`.
   """
-  def discard(game, player, index) do
-    GenServer.call(game, {:discard, player, index})
+  def hint(id, player, to, hint) do
+    PubSub.broadcast(:hanabi, "game:#{id}:plays", {:hint, player, to, hint})
   end
 
   @doc ~S"""
-  One of three play functions for the players.
-
-  `game` is the process identifier, `player` is the player making the play
-  (who must be the current player), and `index` is the zero-based index into
-  the player's hand of the tile they wish to play.  A fresh tile will be
-  drawn into the player's hand at the same position, if one is available.
-
-  Details of the play will be published to each player.
+  Publishes a discard move the `Game` with `id`.
   """
-  def play(game, player, index) do
-    GenServer.call(game, {:play, player, index})
+  def discard(id, player, index) do
+    PubSub.broadcast(:hanabi, "game:#{id}:plays", {:discard, player, index})
+  end
+
+  @doc ~S"""
+  Publishes a play move the `Game` with `id`.
+  """
+  def play(id, player, index) do
+    PubSub.broadcast(:hanabi, "game:#{id}:plays", {:play, player, index})
   end
 
   ### Server ###
@@ -111,243 +100,60 @@ defmodule HanabiEngine.GameManager do
     do: {:stop, "A game requires a List of 2 to 5 players."}
   def init({_game_id, _players, seed})
   when not (is_nil(seed)
-  or (is_tuple(seed) and tuple_size(seed) == 3
-  and (is_integer(elem(seed, 0))
-  and is_integer(elem(seed, 1))
-  and is_integer(elem(seed, 2))))),
+  or (is_tuple(seed) and tuple_size(seed) == 2)
+  and is_atom(elem(seed, 0))
+  and is_list(elem(seed, 1))
+  and is_integer(hd(elem(seed, 1)))
+  and is_integer(tl(elem(seed, 1)))),
     do: {:stop, "A game seed is Tuple of three Integer values."}
   def init({game_id, players, seed}) do
-    if seed do
-      :rand.seed(:exsplus, seed)
-    end
+    if seed, do: :rand.seed(seed)
 
-    all_tiles =
-      for color <- ~w[blue green red white yellow]a,
-        value <- [1, 1, 1, 2, 2, 3, 3, 4, 4, 5] do
-          {color, value}
-      end
-    game =
-      %__MODULE__{
-        id: game_id,
-        players: players,
-        draw_pile: Enum.shuffle(all_tiles),
-        turn: hd(players)
-      }
+    PubSub.subscribe(:hanabi, "game:#{game_id}:plays")
 
-    {:ok, game}
+    {:ok, %__MODULE__{id: game_id, game: Game.new(players)}}
   end
 
   @doc false
-  def handle_call(:deal, _from, game = %__MODULE__{status: :setup}) do
-    player_count = length(game.players)
-    hand_size = if player_count < 4, do: 5, else: 4
-    tile_count = player_count * hand_size
-    tiles =
-      game.draw_pile
-      |> Enum.take(tile_count)
-      |> Enum.chunk(hand_size)
-    hands =
-      game.players
-      |> Enum.zip(tiles)
-      |> Enum.into(%{ })
-    new_draw_pile = Enum.drop(game.draw_pile, tile_count)
-    new_game =
-      %__MODULE__{
-        game |
-        status: :playing,
-        hands: hands,
-        draw_pile: new_draw_pile
-      }
-
-    Enum.each(new_game.players, fn player ->
-      details = %{
-        players: new_game.players,
-        hands: Map.update!(new_game.hands, player, fn hand -> length(hand) end),
-        new_draw_pile: length(new_game.draw_pile),
-        discards: new_game.discards,
-        fireworks: new_game.fireworks,
-        next_turn: new_game.turn,
-        new_clocks: new_game.clocks,
-        new_fuses: new_game.fuses
-      }
-      PubSub.broadcast(
-        :hanabi,
-        "game:#{new_game.id}:#{player}",
-        {:deal, player, details}
-      )
-    end)
-
-    {:reply, :ok, new_game}
+  def handle_info(move = :deal, %__MODULE__{id: id, game: game}) do
+    {reply, new_game} = RulesLawyer.deal_if_legal(game)
+    publish(id, move, reply, new_game)
+    {:noreply, %__MODULE__{id: id, game: new_game}}
   end
-  def handle_call(:deal, _from, game),
-    do: {:reply, {:error, "The game is already setup."}, game}
 
   @doc false
-  def handle_call(
-    {:hint, player, to, hint},
-    _from,
-    game = %__MODULE__{status: :playing, turn: player, clocks: clocks}
-  ) when clocks > 0 do
-    hand = Map.fetch!(game.hands, to)
-    empty = map_hand(hand, 0, hint, nil, nil)
-    {type, positions} =
-      if is_atom(hint) do
-        {:color, map_hand(hand, 0, hint, hint, nil)}
-      else
-        {:value, map_hand(hand, 1, hint, hint, nil)}
-      end
-    if Enum.any?(positions) do
-      new_game = %__MODULE__{
-        game |
-        turn: next_turn(game),
-        clocks: clocks - 1
-      }
-
-      details =
-        %{
-          to: to,
-          color: empty,
-          value: empty,
-          next_turn: new_game.turn,
-          new_clocks: new_game.clocks
-        }
-        |> Map.put(type, positions)
-      Enum.each(new_game.players, fn other_player ->
-        PubSub.broadcast(
-          :hanabi,
-          "game:#{new_game.id}:#{other_player}",
-          {:hint, player, details}
-        )
-      end)
-
-      {:reply, :ok, new_game}
-    else
-      {:reply, {:error, "Invalid hint."}, game}
-    end
-  end
-  def handle_call({:hint, _player, _to, _hint}, _from, game),
-    do: {:reply, {:error, "That player cannot give a hint right now."}, game}
-
-  @doc false
-  def handle_call(
-    {:discard, player, index},
-    _from,
-    game = %__MODULE__{status: :playing, turn: player}
+  def handle_info(
+    move = {:hint, player, to, hint},
+    %__MODULE__{id: id, game: game}
   ) do
-    hand = game.hands |> Map.fetch!(player)
-    discard = hand |> Enum.at(index)
-    if discard do
-      drawn = hd(game.draw_pile)
-      new_hand = List.replace_at(hand, index, drawn)
-      new_game = %__MODULE__{
-        game |
-        hands: Map.put(game.hands, player, new_hand),
-        draw_pile: tl(game.draw_pile),
-        discards: [discard | game.discards],
-        turn: next_turn(game),
-        clocks: Enum.min([game.clocks + 1, 8])
-      }
-
-      details =
-        %{
-          discarded: discard,
-          drawn: drawn,
-          new_draw_pile: length(new_game.draw_pile),
-          next_turn: next_turn(game),
-          new_clocks: new_game.clocks
-        }
-      Enum.each(new_game.players, fn other_player ->
-        PubSub.broadcast(
-          :hanabi,
-          "game:#{new_game.id}:#{other_player}",
-          {:discard, player, details}
-        )
-      end)
-
-      {:reply, :ok, new_game}
-    else
-      {:reply, {:error, "Invalid discard."}, game}
-    end
+    {reply, new_game} = RulesLawyer.hint_if_legal(game, player, to, hint)
+    publish(id, move, reply, new_game)
+    {:noreply, %__MODULE__{id: id, game: new_game}}
   end
-  def handle_call({:discard, _player, _index}, _from, game),
-    do: {:reply, {:error, "That player cannot discard right now."}, game}
 
   @doc false
-  def handle_call(
-    {:play, player, index},
-    _from,
-    game = %__MODULE__{status: :playing, turn: player}
+  def handle_info(
+    move = {:discard, player, index},
+    %__MODULE__{id: id, game: game}
   ) do
-    hand = game.hands |> Map.fetch!(player)
-    tile = hand |> Enum.at(index)
-    if tile do
-      drawn = hd(game.draw_pile)
-      new_hand = List.replace_at(hand, index, drawn)
-      color = elem(tile, 0)
-      fireworks = game.fireworks
-      {new_fireworks, played, discarded, new_fuses} =
-        if (Map.fetch!(fireworks, color) || 0) + 1 == elem(tile, 1) do
-          {
-            Map.put(fireworks, elem(tile, 0), elem(tile, 1)),
-            tile,
-            nil,
-            game.fuses
-          }
-        else
-          {fireworks, nil, tile, game.fuses - 1}
-        end
-      new_discards =
-        if discarded do
-          [discarded | game.discards]
-        else
-          game.discards
-        end
-      new_game = %__MODULE__{
-        game |
-        hands: Map.put(game.hands, player, new_hand),
-        draw_pile: tl(game.draw_pile),
-        discards: new_discards,
-        fireworks: new_fireworks,
-        turn: next_turn(game),
-        fuses: new_fuses
-      }
-
-      details =
-        %{
-          played: played,
-          discarded: discarded,
-          drawn: drawn,
-          new_draw_pile: length(new_game.draw_pile),
-          next_turn: next_turn(game),
-          new_clocks: new_game.clocks,
-          new_fuses: new_game.fuses
-        }
-      Enum.each(new_game.players, fn other_player ->
-        PubSub.broadcast(
-          :hanabi,
-          "game:#{new_game.id}:#{other_player}",
-          {:play, player, details}
-        )
-      end)
-
-      {:reply, :ok, new_game}
-    else
-      {:reply, {:error, "Invalid play."}, game}
-    end
+    {reply, new_game} = RulesLawyer.discard_if_legal(game, player, index)
+    publish(id, move, reply, new_game)
+    {:noreply, %__MODULE__{id: id, game: new_game}}
   end
-  def handle_call({:play, _player, _index}, _from, game),
-    do: {:reply, {:error, "That player cannot make a play right now."}, game}
+
+  @doc false
+  def handle_info(
+    move = {:play, player, index},
+    %__MODULE__{id: id, game: game}
+  ) do
+    {reply, new_game} = RulesLawyer.play_if_legal(game, player, index)
+    publish(id, move, reply, new_game)
+    {:noreply, %__MODULE__{id: id, game: new_game}}
+  end
 
   ### Helpers ###
 
-  defp map_hand(hand, i, test, match, non_match) do
-    Enum.map(hand, fn tile ->
-      if elem(tile, i) == test, do: match, else: non_match
-    end)
-  end
-
-  defp next_turn(game) do
-    i = Enum.find_index(game.players, fn player -> player == game.turn end)
-    Enum.at(game.players, i + 1, hd(game.players))
+  defp publish(id, move, reply, game) do
+    PubSub.broadcast(:hanabi, "game:#{id}:events", {move, reply, game})
   end
 end
--- apps/hanabi_engine/test/game_manager_test.exs
+++ apps/hanabi_engine/test/game_manager_test.exs
@@ -1,8 +1,17 @@
 defmodule GameManagerTest do
   use ExUnit.Case, async: true
 
-  alias Phoenix.PubSub
   alias HanabiEngine.{GameManager, GameSupervisor}
+
+  test "starts new games with an ID and seed" do
+    players = ~w[A B]
+    {:ok, id, ^players, seed} = GameManager.start_new(players)
+    assert is_binary(id) and byte_size(id) > 0
+    assert(
+      is_tuple(seed) and is_atom(elem(seed, 0)) and is_list(elem(seed, 1)) and
+      is_integer(hd(elem(seed, 1))) and is_integer(tl(elem(seed, 1)))
+    )
+  end
 
   test "games are started under dynamic supervision" do
     before_children = Supervisor.which_children(GameSupervisor)
@@ -19,219 +28,69 @@ defmodule GameManagerTest do
     assert child in after_children
   end
 
+  test "the initial deal is triggered by publishing the named message" do
+    {:ok, _game} = GameManager.start("TestDeal", ~w[A B])
+    GameManager.subscribe("TestDeal")
+    GameManager.deal("TestDeal")
+    assert_receive {:deal, :ok, game}
+    assert length(game.draw_pile) == 40
+  end
+
   test "games can be initialized with a seed to make them reproducible" do
-    {:ok, seeded_game} = GameManager.start("TestSeed", ~w[A B], {1, 2, 3})
-    PubSub.subscribe(:hanabi, "game:TestSeed:A")
-    GameManager.deal(seeded_game)
-    assert_receive {
-      :deal,
-      "A",
-      %{hands: %{"B" => [red: 4, blue: 5, red: 2, white: 4, blue: 2]}}
-    }
-  end
-
-  test "the initial deal publishes the game setup for each player" do
-    players = ~w[A B]
-    {:ok, new_game} = GameManager.start("TestDeal", players)
-    PubSub.subscribe(:hanabi, "game:TestDeal:A")
-    GameManager.deal(new_game)
-    assert_receive {
-      :deal,
-      "A",
-      %{
-        players: ^players,
-        hands: %{"A" => 5, "B" => bs_hand},
-        new_draw_pile: 40,
-        discards: [ ],
-        fireworks: %{blue: nil, green: nil, red: nil, white: nil, yellow: nil},
-        next_turn: "A",
-        new_clocks: 8,
-        new_fuses: 3
-      }
-    } when is_list(bs_hand) and length(bs_hand) == 5
-  end
-
-  test "starting hand sizes are based on the number of players" do
-    {:ok, two_player_game} = GameManager.start("TestTwoPlayerHand", ~w[A B])
-    PubSub.subscribe(:hanabi, "game:TestTwoPlayerHand:A")
-    GameManager.deal(two_player_game)
-    assert_receive {:deal, "A", %{hands: %{"A" => 5}}}
-
-    {:ok, four_player_game} = GameManager.start("TestFourPlayerHand", ~w[A B C D])
-    PubSub.subscribe(:hanabi, "game:TestFourPlayerHand:A")
-    GameManager.deal(four_player_game)
-    assert_receive {:deal, "A", %{hands: %{"A" => 4}}}
+    seed = :rand.seed_s(:exsplus, {1, 2, 3}) |> :rand.export_seed_s
+    {:ok, _game} = GameManager.start("TestSeed", ~w[A B], seed)
+    GameManager.subscribe("TestSeed")
+    GameManager.deal("TestSeed")
+    assert_receive {:deal, :ok, game}
+    bs_hand = game.hands |> Map.fetch!("B")
+    assert bs_hand == [red: 4, blue: 5, red: 2, white: 4, blue: 2]
   end
 
   describe "moves" do
     setup do
       game_id = UUID.uuid1
-      {:ok, game} = GameManager.start(game_id, ~w[A B])
-      PubSub.subscribe(:hanabi, "game:#{game_id}:A")
-      GameManager.deal(game)
-      details =
-        receive do
-          {:deal, "A", deal} -> deal
-        after
-          100 -> raise "Hand not received within 100ms."
-        end
-      {:ok, game: game, details: details}
+      # Use a known deal for testing purposes:
+      #
+      # * A's hand:  `[green: 4, green: 3, white: 3, blue: 4, green: 1]`
+      # * B's hand:  `[red: 4, blue: 5, red: 2, white: 4, blue: 2]`
+      # * Top three draws:  `[green: 4, white: 4, red: 5, …]`
+      seed = :rand.seed_s(:exsplus, {1, 2, 3}) |> :rand.export_seed_s
+      {:ok, _game} = GameManager.start(game_id, ~w[A B], seed)
+      GameManager.subscribe(game_id)
+      GameManager.deal(game_id)
+      assert_receive {:deal, :ok, game}
+      {:ok, id: game_id, game: game}
     end
 
-    test "giving a hint", %{game: game, details: details} do
-      bs_hand = Map.fetch!(details.hands, "B")
-      hint_color = bs_hand |> hd |> elem(0)
-      hint = Enum.map(bs_hand, fn {tile_color, _} ->
-        if tile_color == hint_color do
-          hint_color
-        else
-          nil
-        end
-      end)
-      :ok = GameManager.hint(game, "A", "B", hint_color)
-      assert_receive {
-        :hint,
-        "A",
-        %{
-          to: "B",
-          color: ^hint,
-          value: [nil, nil, nil, nil, nil],
-          next_turn: "B",
-          new_clocks: 7
-        }
-      }
-
-      value = Enum.find(1..5, fn n -> GameManager.hint(game, "B", "A", n) == :ok end)
-      assert_receive {
-        :hint,
-        "B",
-        %{
-          to: "A",
-          color: [nil, nil, nil, nil, nil],
-          value: positions,
-          next_turn: "A",
-          new_clocks: 6
-        }
-      }
-      assert Enum.find(positions, fn position -> position == value end)
+    test "hints are given by publishing the named message", %{id: id} do
+      GameManager.hint(id, "A", "B", :red)
+      assert_receive {{:hint, "A", "B", :red}, :ok, game}
+      bs_knowns = game.knowns |> Map.fetch!("B")
+      hint = [{:red, nil}, {nil, nil}, {:red, nil}, {nil, nil}, {nil, nil}]
+      assert bs_knowns == hint
     end
 
-    test "discarding a tile", %{game: game, details: details} do
-      # spend a clock
-      Enum.find(1..5, fn n -> GameManager.hint(game, "A", "B", n) == :ok end)
-      assert_receive {:hint, "A", %{new_clocks: 7}}
-
-      discard = details.hands |> Map.fetch!("B") |> Enum.at(1)
-      :ok = GameManager.discard(game, "B", 1)
-      assert_receive {
-        :discard,
-        "B",
-        %{
-          discarded: ^discard,
-          drawn: drawn,
-          new_draw_pile: 39,
-          next_turn: "A",
-          new_clocks: 8
-        }
-      }
-      assert is_tuple(drawn) and
-             tuple_size(drawn) == 2 and
-             elem(drawn, 0) in ~w[blue green red white yellow]a and
-             elem(drawn, 1) in 1..5
-
-      :ok = GameManager.discard(game, "A", 0)
-      assert_receive {
-        :discard,
-        "A",
-        %{
-          discarded: unknown,
-          drawn: drawn,
-          new_draw_pile: 38,
-          next_turn: "B",
-          new_clocks: 8
-        }
-      }
-      assert is_tuple(unknown) and
-             tuple_size(unknown) == 2 and
-             elem(unknown, 0) in ~w[blue green red white yellow]a and
-             elem(unknown, 1) in 1..5
-      assert is_tuple(drawn) and
-             tuple_size(drawn) == 2 and
-             elem(drawn, 0) in ~w[blue green red white yellow]a and
-             elem(drawn, 1) in 1..5
+    test "discards are made by publishing the named message", %{id: id} do
+      GameManager.discard(id, "A", 0)
+      assert_receive {{:discard, "A", 0}, :ok, game}
+      assert game.discards == [green: 4]
     end
 
-    test "playing a tile", %{game: game, details: details} do
-      :ok = GameManager.play(game, "A", 0)
-      assert_receive {
-        :play,
-        "A",
-        %{
-          played: first_play,
-          discarded: first_discard,
-          drawn: drawn,
-          new_draw_pile: 39,
-          next_turn: "B",
-          new_clocks: 8,
-          new_fuses: first_fuses
-        }
-      }
-      assert is_tuple(drawn) and
-             tuple_size(drawn) == 2 and
-             elem(drawn, 0) in ~w[blue green red white yellow]a and
-             elem(drawn, 1) in 1..5
-      if is_tuple(first_play) do
-        assert tuple_size(first_play) == 2 and
-               elem(first_play, 0) in ~w[blue green red white yellow]a and
-               elem(first_play, 1) in 1..5
-        assert is_nil(first_discard)
-        assert first_fuses == 3
-      else
-        assert is_nil(first_play)
-        assert tuple_size(first_discard) == 2 and
-               elem(first_discard, 0) in ~w[blue green red white yellow]a and
-               elem(first_discard, 1) in 1..5
-        assert first_fuses == 2
-      end
+    test "plays are made by publishing the named message", %{id: id} do
+      GameManager.play(id, "A", 4)
+      assert_receive {{:play, "A", 4}, :ok, game}
+      assert game.fireworks.green == 1
+    end
 
-      bs_hand = details.hands |> Map.fetch!("B")
-      legal_play =
-        Enum.find(bs_hand, fn tile ->
-          tile != first_play and elem(tile, 1) == 1
-        end)
-      second_discard = if legal_play, do: nil, else: hd(bs_hand)
-      index = Enum.find_index(bs_hand, fn tile -> legal_play == tile end)
-      :ok = GameManager.play(game, "B", index || 0)
+    test "illegal moves receive an error reply and an unchanged game",
+         %{id: id, game: dealt_game} do
+      GameManager.hint(id, "B", "A", 1)
       assert_receive {
-        :play,
-        "B",
-        %{
-          played: ^legal_play,
-          discarded: ^second_discard,
-          drawn: drawn,
-          new_draw_pile: 38,
-          next_turn: "A",
-          new_clocks: 8,
-          new_fuses: second_fuses
-        }
+        {:hint, "B", "A", 1},
+        {:error, "It's not B's turn."},
+        game
       }
-      assert is_tuple(drawn) and
-             tuple_size(drawn) == 2 and
-             elem(drawn, 0) in ~w[blue green red white yellow]a and
-             elem(drawn, 1) in 1..5
-      if is_tuple(legal_play) do
-        assert tuple_size(legal_play) == 2 and
-               elem(legal_play, 0) in ~w[blue green red white yellow]a and
-               elem(legal_play, 1) in 1..5
-        assert is_nil(second_discard)
-        assert second_fuses == first_fuses
-      else
-        assert is_nil(legal_play)
-        assert tuple_size(second_discard) == 2 and
-               elem(second_discard, 0) in ~w[blue green red white yellow]a and
-               elem(second_discard, 1) in 1..5
-        assert second_fuses == first_fuses - 1
-      end
+      assert dealt_game == game
     end
   end
 end
